ba9e51860b6bfb6116b29bb41b149b3d7c15ae8d
misha-solntcev <misha.solntcev@gmail.com>
2022-12-22 13:51:48 +0800

    Миша и негатив

--- List.py
+++ List.py
@@ -1,14 +1,16 @@
+n, m = map(int, input().split())
 a = []
-for i in range(4):
+for i in range(n):
     a.append(list(map(str, input())))
+input()
+b = []
+for i in range(n):
+    b.append(list(map(str, input())))
 
 count = 0
-for i in range(3):
-    for j in range(3):
-        if a[i][j] == a[i][j+1] == a[i+1][j+1] == a[i+1][j]:
+for i in range(n):
+    for j in range(m):
+        if a[i][j] == b[i][j]:
             count += 1
 
-if count == 0:
-    print("Yes")
-else:
-    print("No")
+print(count)
