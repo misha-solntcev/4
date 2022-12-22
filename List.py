ba3293d964e115996eeaa8c04a5d07b84de7ca7d
misha-solntcev <misha.solntcev@gmail.com>
2022-12-22 13:44:27 +0800

    Симпатичный узор

--- List.py
+++ List.py
@@ -1,26 +1,14 @@
-# N, M = map(int, input().split())
-# a = []
-# for i in range(5):
-#     a.append(list(map(int, input().split())))
-N = 3
-M = 4
-a = [
-    [5, 9, 2, 6],
-    [6, 2, 4, 3],
-    [1, 2, 8, 7]
-]
+a = []
+for i in range(4):
+    a.append(list(map(str, input())))
 
-sum_N = []
-for i in range(N):
-    s_N = 0
-    for j in range(M):
-        s_N += a[i][j]
-    sum_N.append(s_N)
-print(sum_N)
-sum_M = []
-for j in range(M):
-    s_M = 0
-    for i in range(N):
-        s_M += a[i][j]
-    sum_M.append(s_M)
-print(sum_M)
+count = 0
+for i in range(3):
+    for j in range(3):
+        if a[i][j] == a[i][j+1] == a[i+1][j+1] == a[i+1][j]:
+            count += 1
+
+if count == 0:
+    print("Yes")
+else:
+    print("No")
