8fc29e83e2ce13c736119bdf2015a6ec181972f7
misha-solntcev <misha.solntcev@gmail.com>
2022-12-23 12:20:26 +0800

    Фотографии Брейна

--- 5.8 List 2.py
+++ 5.8 List 2.py
@@ -1,26 +1,18 @@
 n, m = map(int, input().split())
 A = []
 
-start = 0
-stop = m
-step = 1
 for i in range(n):
-    B = []
-    for j in range(start, stop, step):
-        B.append(j)
-    A.append(B)
-    start += m
-    stop += m
-    if i % 2 == 0:
-        start = start - 1
-        stop = stop - 1
-    else:
-        start = start + 1
-        stop = stop + 1
-    start, stop = stop, start
-    step = - step
+    A.append(list(map(str, input().split())))
 
+black = ['W', 'G', 'B']
+b = 0
+color = ['C', 'M', 'Y']
+c = 0
 for i in range(n):
     for j in range(m):
-        print(A[i][j], end=" ")
-    print()
+        if a[i][j] in color:
+            c += 1
+if c > 0:
+    print("#Color")
+else:
+    print("#Black&White")
