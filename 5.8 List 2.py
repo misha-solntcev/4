30f9872fbf74a16c859a483aaf138602cad33c16
misha-solntcev <misha.solntcev@gmail.com>
2022-12-23 12:11:19 +0800

    Заполнение змейкой

--- 5.8 List 2.py
+++ 5.8 List 2.py
@@ -1,12 +1,26 @@
 n, m = map(int, input().split())
-a = ['.' * (m+2)]
-for i in range(n):
-    a.append('.' + input() + '.')
-a.append('.' * (m+2))
+A = []
 
-count = 0
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        if a[i][j] == a[i][j-1] == a[i][j+1] == a[i-1][j] == a[i+1][j] == '.':
-            count += 1
-print(count)
+start = 0
+stop = m
+step = 1
+for i in range(n):
+    B = []
+    for j in range(start, stop, step):
+        B.append(j)
+    A.append(B)
+    start += m
+    stop += m
+    if i % 2 == 0:
+        start = start - 1
+        stop = stop - 1
+    else:
+        start = start + 1
+        stop = stop + 1
+    start, stop = stop, start
+    step = - step
+
+for i in range(n):
+    for j in range(m):
+        print(A[i][j], end=" ")
+    print()
